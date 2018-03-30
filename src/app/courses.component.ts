40e1e5c85fe20e8a2fba087a9d631e017c203f57
Tabuzo, Elmo Jr V <elmo.jr.v.tabuzo@accenture.com>
2018-03-30 23:32:04 +0800

    Edited for Directives Tutorial

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,14 +3,20 @@ import { Component } from '@angular/core';
 //decorator function
 @Component({
     selector: 'courses', // <div class="courses" will be ".courses"
-    template: '<h2>{{ getTitle() }}</h2>' //this will render the app.component.html using <courses></courses>
+
+    //this will render the app.component.html using <courses></courses>
+    template: `
+        <h2>{{ title }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+        ` 
 })
 
 // we need to export the class in order to see it in angular
 export class CoursesComponent {
     title = "List of Courses";
-
-    getTitle() {
-        return this.title;
-    }
+    courses = ["course1", "course2", "course3"];
 }
