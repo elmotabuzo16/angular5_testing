53f6ea07f0c140ad627faacc790a93a45319b81e
Tabuzo, Elmo Jr V <elmo.jr.v.tabuzo@accenture.com>
2018-03-30 23:24:13 +0800

    Added some logic

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 //decorator function
 @Component({
     selector: 'courses', // <div class="courses" will be ".courses"
-    template: '<h2>Courses</h2>' //this will render the app.component.html using <courses></courses>
+    template: '<h2>{{ getTitle() }}</h2>' //this will render the app.component.html using <courses></courses>
 })
 
 // we need to export the class in order to see it in angular
 export class CoursesComponent {
+    title = "List of Courses";
 
+    getTitle() {
+        return this.title;
+    }
 }
