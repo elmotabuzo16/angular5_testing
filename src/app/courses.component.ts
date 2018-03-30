fccb95ecdc4cc0867b2bf7c31da73fcf00ecdadf
Tabuzo, Elmo Jr V <elmo.jr.v.tabuzo@accenture.com>
2018-03-31 00:37:41 +0800

    Adding bootstrap $npm install bootstrap --save

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -8,31 +8,10 @@ import { CoursesService } from './courses.service';
     //this will render the app.component.html using <courses></courses>
     //<h2 [textContent]="title" />
     template: `
-        <h2>{{ title }}</h2>
-        <ul>
-            <li *ngFor="let course of courses">
-                {{ course }}
-            </li>
-        </ul>
-
-        <table>
-            <tr>
-                <td [attr.colspan]="colSpan">Sample of Attribute Binding</td>
-            </tr>
-        </table>
+        <button class="btn btn-primary">Save</button>
         ` 
 })
 
 // we need to export the class in order to see it in angular
 export class CoursesComponent {
-    title = "List of Courses";
-    courses;
-    colSpan = 2;
-
-    //Auto instantiate the CoursesService
-    //After this, add the CoursesSerivce in provider[] under app.module.ts
-    //Singleton
-    constructor(service: CoursesService) {
-         this.courses = service.getCourses();
-    }
 }
