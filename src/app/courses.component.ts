b6835c73e89e801eb7c86a41c39309d4ba6d4a7d
Tabuzo, Elmo Jr V <elmo.jr.v.tabuzo@accenture.com>
2018-03-31 11:59:28 +0800

    Pipe

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,19 +5,23 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses', // <div class="courses" will be ".courses"
 
-    //this will render the app.component.html using <courses></courses>
-    //<h2 [textContent]="title" />
+    
     template: `
-        <input [value]="email" (keyup.enter)="email = $event.target.value; onKeyUp()" />
-        <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
+        {{ courses.title | uppercase }} <br/>
+        {{ courses.rating | number:'2.2-2' }} <br/>
+        {{ courses.students | number }} <br/>
+        {{ courses.price | currency:'AUD':true:'3.2-2' }} <br/>
+        {{ courses.releseDate | date:'shortdate' }} <br/>
         ` 
 })
 
 // we need to export the class in order to see it in angular
 export class CoursesComponent {
-    email = "me@example.com"
-
-    onKeyUp(email) {
-        console.log(this.email);
+    courses = {
+        title: "The Complete Angular Course",
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releseDate: new Date(2018, 3, 31)
     }
 }
