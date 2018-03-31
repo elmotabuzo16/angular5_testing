d4e8eb99583420fd7d67d95b86888f07241e82fb
Tabuzo, Elmo Jr V <elmo.jr.v.tabuzo@accenture.com>
2018-03-31 11:50:02 +0800

    Two-Way Binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -8,13 +8,16 @@ import { CoursesService } from './courses.service';
     //this will render the app.component.html using <courses></courses>
     //<h2 [textContent]="title" />
     template: `
-        <input #email (keyup.enter)="onKeyUp(email.value)" />
+        <input [value]="email" (keyup.enter)="email = $event.target.value; onKeyUp()" />
+        <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
         ` 
 })
 
 // we need to export the class in order to see it in angular
 export class CoursesComponent {
+    email = "me@example.com"
+
     onKeyUp(email) {
-        console.log(email);
+        console.log(this.email);
     }
 }
